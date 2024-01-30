ccc605a4551f445d2b3b877aefb84d70fd305261
D'Amico Salvatore Alessandro <122369512+Frelser77@users.noreply.github.com>
2024-01-30 15:57:47 +0100

    Aggiungere i file di progetto.

---
+++ ConsoleApp3/Class1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    internal class Persona
+    {
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public int Eta { get; set; }
+
+        public Persona(string nome)
+        {
+            Nome = nome;
+        }
+
+        public Persona(int eta)
+        {
+            Eta = eta;
+        }
+        public Persona(string nome, string cognome)
+        {
+            Nome = nome;
+            Cognome = cognome;
+        }
+        public Persona(string nome, string cognome, int eta)
+        {
+            Nome = nome;
+            Cognome = cognome;
+            Eta = eta;
+        }
+
+        public string GetNome()
+        {
+            return Nome;
+        }
+
+        public string GetCognome()
+        {
+            return Cognome;
+        }
+
+        public int GetEta()
+        {
+            return Eta;
+        }
+
+        public string GetDetails()
+        {
+            return "Il nome é " + GetNome() + ", il suo cognome é " + GetCognome() + " e la sua etá é " + GetEta();
+        }
+
+        public void VerificaEta()
+        {
+            if (Eta >= 18)
+            {
+                Console.WriteLine("Sei maggiorenne puoi proseguire oltre, Utente Verificato!");
+                Console.WriteLine("Premi un tasto per continuare");
+            }
+            else if (Eta == 17)
+            {
+                Console.WriteLine("Mi dispiace, non puoi proseguire oltre torna tra un anno per sapere cosa nascondiamo");
+                Console.WriteLine("Ciao a presto");
+            }
+            else
+            {
+                Console.WriteLine("Mi dispiace, non puoi proseguire oltre torna tra " + (18 - Eta) + " un anni per sapere cosa nascondiamo");
+                Console.WriteLine("Ciao ci rivedremo quando crescerai!");
+            }
+        }
+
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* SENZA IMPUT, STATICA   */
+            Persona persona1 = new Persona("Salvatore");
+            Persona persona2 = new Persona("Salvatore", "D'Amico");
+            Persona persona3 = new Persona("Salvatore", "D'Amico", 27);
+
+            Console.WriteLine("\t" + "\t" + " SECONDA CONSOLE " + "\t" + "\t");
+            Console.WriteLine("Creato con costruttori");
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Nome, preso con GetNome");
+            Console.WriteLine("\t" + "\t" + persona1.GetNome() + "\t" + "\t");
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Nome e " + "Cognome, presi con GetNome e GetCognome");
+            Console.WriteLine("\t" + "\t" + "\t" + persona2.GetNome() + "\t" + persona2.GetCognome() + "\t" + "\t");
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Dettagli completi presi con GetDetails");
+            Console.WriteLine("\t" + persona3.GetDetails() + " anni" + "\t" + "\t");
+            Console.WriteLine("\n");
+
+
+            /////////////////////////////////////////////////////////////////////////////////////
+            // Richiamando i metodi
+
+            Console.WriteLine("\n");
+            Persona metodoNome = new Persona("Solo nome");
+            Persona metodoCognome = new Persona("Nome e ", "Cognome");
+            Persona metodoEta = new Persona(27);
+            Persona metodoDetails = new Persona("Nome", "Cognome", 27);
+
+            Console.WriteLine(metodoNome.GetNome());
+            Console.WriteLine(metodoCognome.GetCognome());
+            Console.WriteLine(metodoEta.GetEta());
+            Console.WriteLine(metodoDetails.GetDetails());
+            Console.WriteLine("\n");
+
+
+            //////////////////////////////////////////////////////////////////////////
+            //CON IMPUT
+            Console.WriteLine("Con richiesta di imput per proseguire");
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Inserisci il tuo nome");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Inserisci il tuo cognome");
+            string cognome = Console.ReadLine();
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Inserisci la tua età");
+            int eta;
+            while (!int.TryParse(Console.ReadLine(), out eta))
+            {
+                Console.WriteLine("Inserire un valore nuemrico");
+            }
+
+            Console.WriteLine("\n");
+
+            // inizializzo
+            Persona persona = new Persona(nome, cognome, eta);
+
+            // Mostro la classe persona inizializzata sopra
+            Console.WriteLine(persona.GetDetails());
+            // Verifica Eta
+            persona.VerificaEta();
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
